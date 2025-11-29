66e3fb62494a5c7c1c4a58ae28a15bf85abde22b
ひばら <147604079+RosyVein@users.noreply.github.com>
2025-11-29 16:48:08 +0800

    Apple Silicon support

---
+++ README.md
@@ -0,0 +1,11 @@
+# creversi-apple-silicon: A fast Python Reversi library work on Apple Silicon
+
+## Summary
+
+creversi-apple-silicon is base on original creversi library and includes modifications to enable compatibility with Apple Silicon (ARM64) systems.
+
+For usage and other details about the original work, please refer to the upstream repository.
+
+## License
+
+The source code remains licensed under GPLv3, in accordance with the original project.
--- setup.py
+++ setup.py
@@ -1,15 +1,37 @@
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import numpy
+import os
+import platform
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+
+include_dirs = [
+    os.path.join(ROOT, "creversi_cpp"),
+    os.path.join(ROOT, "simde"),   # simde submodule
+    numpy.get_include(),
+]
 
 class my_build_ext(build_ext):
     def build_extensions(self):
-        if self.compiler.compiler_type == 'unix':
-            for e in self.extensions:
-                e.extra_compile_args = ['-std=c++11', '-msse4.2', '-mbmi', '-mbmi2', '-mavx2']
-        elif self.compiler.compiler_type == 'msvc':
-            for e in self.extensions:
-                e.extra_compile_args = ['/arch:AVX2']
+        machine = platform.machine().lower()
+        compiler = self.compiler.compiler_type
+
+        for e in self.extensions:
+            if compiler == 'unix':
+                # Intel Mac or Linux x86_64
+                if machine == 'x86_64':
+                    e.extra_compile_args = ['-std=c++11', '-msse4.2', '-mbmi', '-mbmi2', '-mavx2']
+                elif machine == 'aarch64' or machine == 'arm64':
+                    # ARM64 (Apple Silicon or Linux ARM)
+                    e.extra_compile_args = ['-std=c++11']
+
+            elif compiler == 'msvc':
+                # Windows x86
+                if machine == 'amd64':
+                    e.extra_compile_args = ['/arch:AVX2']
+                else:
+                    e.extra_compile_args = []
 
         build_ext.build_extensions(self)
 
@@ -18,7 +40,7 @@ ext_modules = [
         ['creversi/creversi.pyx',
          'creversi_cpp/bit_manipulations.cpp', 'creversi_cpp/hand.cpp', 'creversi_cpp/movable_generator.cpp', 'creversi_cpp/move_generator.cpp', 'creversi_cpp/state.cpp', 'creversi_cpp/utils.cpp', 'creversi_cpp/value.cpp'],
         language='c++',
-        include_dirs = ['creversi_cpp', numpy.get_include()]),
+        include_dirs = include_dirs),
     Extension('creversi.gym_reversi.envs.reversi_env',
         ['creversi/gym_reversi/envs/reversi_env.pyx'],
         language='c++'),
